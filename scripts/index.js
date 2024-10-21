8eda22562313285b37b0b2b6367496aa38cfc1ad
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2024-10-21 00:43:30 -0700

    added remove buttons

--- scripts/index.js
+++ scripts/index.js
@@ -35,6 +35,11 @@ const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
+const profileEditform = profileEditModal.querySelector(".modal__form");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -44,5 +49,12 @@ profileEditButton.addEventListener("click", () => {
 });
 
 profileRemoveModal.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+  closePopup();
+});
+
+profileEditform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
 });
