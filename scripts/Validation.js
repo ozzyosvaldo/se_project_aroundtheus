c1721aba2d80901258bee7a145fa945edac07618
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-03-11 22:16:33 -0700

    addeing JS validation

--- scripts/Validation.js
+++ scripts/Validation.js
@@ -1,1 +1,18 @@
-console.log("hello from js");
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function enableValidation(config) {
+  const formEls = Array.from(document.querySelectorAll(".modal__form"));
+  console.log(formEls);
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
