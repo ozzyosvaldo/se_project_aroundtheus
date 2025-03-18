320111659936a720a0b651e152776bd9797676f0
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-03-18 15:47:04 -0700

    added fromEL

--- scripts/Validation.js
+++ scripts/Validation.js
@@ -1,15 +1,26 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function setEventListeners(formEl, options) {}
+
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(".modal__form")];
+  const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
-    formEl.addEventListener("Submit" () =>{});
+    formEl.addEventListener("Submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formEl, options);
+    //look for all inputs inside of form
+    // loop though all inputs to see if they are valid
+    // if input is not valid
+    //get validation message
+    //add error class to input
+    //display error message
   });
 }
 
 const config = {
-  formSelector: ".popup__form",
+  formSelector: ".modal__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
