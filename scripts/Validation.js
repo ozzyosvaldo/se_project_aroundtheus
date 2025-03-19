ac6f2e4ce2d1bc2ea08a8f4c14c28e59e05e2aa1
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-03-19 12:56:12 -0700

    added error message for name

--- scripts/Validation.js
+++ scripts/Validation.js
@@ -1,7 +1,15 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-function setEventListeners(formEl, options) {}
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      console.dir(inputEl);
+    });
+  });
+}
 
 function enableValidation(options) {
   const formEls = [...document.querySelectorAll(options.formSelector)];
@@ -21,8 +29,8 @@ function enableValidation(options) {
 
 const config = {
   formSelector: ".modal__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
