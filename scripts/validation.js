b0015d51c4ee907724ead77062aca629daefbdf0
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-03-20 07:37:51 -0700

    fixed text

--- scripts/validation.js
+++ scripts/validation.js
@@ -45,6 +45,7 @@ function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   const submitButton = formEl.querySelector(".modal__button");
+
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
