651d69fd0cb993a7175eca33860faaf9646e81ce
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-07-11 16:41:33 -0700

    added form validator

---
+++ scripts/Card.js
@@ -0,0 +1,44 @@
+export default class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  }
+
+  _setEventListeners() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon();
+      });
+
+    //.card__like-button
+    //card__delete-button
+
+    const deleteButton = this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCard();
+      });
+  }
+
+  _handleDeleteCard() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle(".card__like-button_is-active");
+  }
+
+  getview() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    this._setEventListeners();
+  }
+}
