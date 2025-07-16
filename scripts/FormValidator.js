f751733e6011c6ee8efff9f2c3750ff1f21b7b82
OsvaldoPozo <Ozzyosvaldo@icloud.com>
2025-07-16 14:25:11 -0700

    updated

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -15,29 +15,60 @@ class FormValidator {
     errorMessageEl.classList.add(this._errorClass);
   }
 
-  _toggleButtonState() {}
+  _hideInputError(inputEl) {
+    const errorMessageEl = this._form.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.remove(this._inputErrorClass);
+    errorMessageEl.textContent = "";
+    errorMessageEl.classList.remove(this._errorClass);
+  }
 
-  _hasInvalidInput() {}
+  _checkInputValidity(inputEl) {
+    if (!inputEl.validity.valid) {
+      this._showInputError(inputEl);
+    } else {
+      this._hideInputError(inputEl);
+    }
+  }
 
-  _checkInputValidity() {}
+  _hasInvalidInput() {
+    return this._inputEls.some((inputEl) => !inputEl.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._submitButton.classList.add(this._inactiveButtonClass);
+      this._submitButton.disabled = true;
+    } else {
+      this._submitButton.classList.remove(this._inactiveButtonClass);
+      this._submitButton.disabled = false;
+    }
+  }
 
   _setEventListeners() {
     this._inputEls = [...this._form.querySelectorAll(this._inputSelector)];
     this._submitButton = this._form.querySelector(this._submitButtonSelector);
 
-    inputEls.forEach((inputEl) => {
+    this._inputEls.forEach((inputEl) => {
       inputEl.addEventListener("input", () => {
-        checkInputValidity(inputEl);
-        toggleButtonState();
+        this._checkInputValidity(inputEl);
+        this._toggleButtonState();
       });
     });
   }
 
   enableValidation() {
-    this._form.addEventListener("Submit", (e) => {
+    this._form.addEventListener("submit", (e) => {
       e.preventDefault();
     });
-    setEventListeners(formEl, options);
+    this._setEventListeners();
+    this._toggleButtonState(); // ensure initial state is correct
+  }
+
+  resetValidation() {
+    this._inputEls.forEach((inputEl) => {
+      this._hideInputError(inputEl);
+    });
+    this._toggleButtonState();
   }
 }
 
